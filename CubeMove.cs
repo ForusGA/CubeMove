da9b15b4f92264adaa2aa2f22638d327b3ac6200
Forus <52991400+ForusGA@users.noreply.github.com>
2023-10-22 14:45:21 +0500

    Create CubeMove.cs

---
+++ CubeMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : MonoBehaviour
+{
+    int health = 5;
+    int level = 1;
+    float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print($"Здровье: {health}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = this.transform.position;
+        currentPosition.z += speed * Time.deltaTime;
+        transform.position = currentPosition;
+    }
+}
